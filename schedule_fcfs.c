cb357961e05fa73b168ae6307f1a437f4a44a133
BIDL8588 <lopezbyron8588@gmail.com>
2024-11-08 13:19:11 -0800

    Update schedule_fcfs.c

--- schedule_fcfs.c
+++ schedule_fcfs.c
@@ -10,3 +10,36 @@
 #include "cpu.h"
 
 //Your code and design here
+void waitingTime(Task myTask[],int n)
+    {
+        int waitingTi[n]; 
+
+        for(int i = 1; i < n; i ++)
+        {
+            waitingTi[i]= myTask[i - 1].burst + waitingTi [i-1];
+            myTask[i].tid = i;
+        }
+
+        for(int i = 1; i < n; i++)
+        {
+            printf("Task %d: Waiting Time = %d \n", myTask[i].tid, waitingTi[i]); 
+        }
+    }
+
+void averagewaitingTime(Task myTask[], int n)
+    {
+        int averageTime; 
+        int totalWait = 0; 
+        int waitingTime =0; 
+
+        for(int i = 1; i < n; i ++)
+        {
+
+            waitingTime += myTask[i-1].burst;
+            totalWait += waitingTime;
+        }
+
+        averageTime = totalWait/n;
+        printf("Average waiting time: ", averageTime); 
+        
+    }
