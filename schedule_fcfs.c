f8b918f9209c17549958bad8a3e941a2cf67c223
BIDL8588 <lopezbyron8588@gmail.com>
2024-11-08 20:58:46 -0800

    Update schedule_fcfs.c

--- schedule_fcfs.c
+++ schedule_fcfs.c
@@ -8,38 +8,70 @@
 #include "task.h"
 #include "list.h"
 #include "cpu.h"
-
+#include <string.h>
 //Your code and design here
-void waitingTime(Task myTask[],int n)
-    {
-        int waitingTi[n]; 
 
-        for(int i = 1; i < n; i ++)
-        {
-            waitingTi[i]= myTask[i - 1].burst + waitingTi [i-1];
-            myTask[i].tid = i;
-        }
+struct node *head = NULL; 
 
-        for(int i = 1; i < n; i++)
-        {
-            printf("Task %d: Waiting Time = %d \n", myTask[i].tid, waitingTi[i]); 
-        }
+void add(char *name, int priority, int burst) {
+    Task *newTask = (Task *)malloc(sizeof(Task));
+    if (newTask == NULL) {
+        fprintf(stderr, "Error: Could not allocate memory for new task\n");
+        return;
     }
 
-void averagewaitingTime(Task myTask[], int n)
-    {
-        int averageTime; 
-        int totalWait = 0; 
-        int waitingTime =0; 
-
-        for(int i = 1; i < n; i ++)
-        {
-
-            waitingTime += myTask[i-1].burst;
-            totalWait += waitingTime;
-        }
-
-        averageTime = totalWait/n;
-        printf("Average waiting time: ", averageTime); 
-        
+    newTask->name = (char *)malloc(strlen(name) + 1);
+    if (newTask->name != NULL) {
+        strcpy(newTask->name, name);
+    } else {
+        fprintf(stderr, "Error: Could not allocate memory for task name\n");
+        free(newTask);
+        return;
     }
+
+    newTask->priority = priority;
+    newTask->burst = burst;
+    newTask->tid = 0; 
+    struct node *newNode = (struct node*)malloc(sizeof(struct node));
+    if (newNode == NULL) {
+        fprintf(stderr, "Error: Could not allocate memory for new node\n");
+        free(newTask->name);
+        free(newTask);
+        return;
+    }
+    newNode->task = newTask;
+    newNode->next = NULL;
+
+    if (head == NULL) {
+        head = newNode;
+    } else {
+        struct node *current = head;
+        while (current->next != NULL) {
+            current = current->next;
+        }
+        current->next = newNode;
+    }
+}
+void schedule()
+{
+    struct node *current = head; 
+    int totalWait = 0; 
+    int waitingTime =0; 
+    int count = 0; 
+    while(current != NULL)
+    {
+        printf("Task %s: Waiting Time = %d\n", current->task->name, waitingTime);
+        totalWait += waitingTime; 
+
+        waitingTime += current->task->burst; 
+
+        current = current->next; 
+        count++; 
+    }
+    if (count > 0)
+    {
+        float averageWait = (float)totalWait/ count; 
+        printf("Average Waiting Time: %.2f\n", averageWait);
+    }  
+}
+
